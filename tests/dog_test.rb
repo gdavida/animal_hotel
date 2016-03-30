d9d30f9203b6f1eb9000458e6c04dd25cda66bce
Davida Gaffney <gdavida@gmail.com>
2016-03-30 16:10:11 -0500

    added ActiveRecord validations to Dog

--- tests/dog_test.rb
+++ tests/dog_test.rb
@@ -1,74 +1,84 @@
-require_relative "../lib/dog.rb"
 require_relative "test_helper.rb"
+require_relative "../lib/dog.rb"
 
 class DogTest < Minitest::Test 
 
 
-  #INITIAL TEST
-  #---------------------------
-  def test_can_be_created_under_normal_circumstances
-    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "10", sex: "M")
-    refute_nil(dog)
+  # 
+  #---------------------------------------------------------  
+  def test_validator_should_fail_with_no_name
+    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "10", sex: "M", good_with_kids: true)
+    assert(dog.valid?, "Should be valid at creation")
+
+    dog.name = ""
+    refute(dog.valid?, "Should be invalid with an empty name")
+
+    dog.name = nil
+    refute(dog.valid?, "Should be invalid without a name")
   end
+ 
+  # 
+  #---------------------------------------------------------  
+  def test_validator_should_fail_with_no_breed
+    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "10", sex: "M", good_with_kids: true)
+    assert(dog.valid?, "Should be valid at creation")
 
-  #GETTER TESTS
-  #---------------------------
+    dog.breed = ""
+    refute(dog.valid?, "Should be invalid with an empty breed")
 
-  def test_name_getter
-    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "10", sex: "M")
-    assert_equal("Henry", dog.name)
+    dog.breed = nil
+    refute(dog.valid?, "Should be invalid without a breed")
   end
+ 
+  # 
+  #---------------------------------------------------------  
+  def test_validator_should_fail_with_no_size
+    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "10", sex: "M", good_with_kids: true)
+    assert(dog.valid?, "Should be valid at creation")
 
-  def test_breed_getter
-    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "10", sex: "M")
-    assert_equal("basset hound", dog.breed)
+    dog.size = ""
+    refute(dog.valid?, "Should be invalid with an empty size")
+
+    dog.size = nil
+    refute(dog.valid?, "Should be invalid without a size")
   end
+ 
+  # 
+  #---------------------------------------------------------  
+  def test_validator_should_fail_with_no_age
+    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "10", sex: "M", good_with_kids: true)
+    assert(dog.valid?, "Should be valid at creation")
 
-  def test_size_getter
-    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "10", sex: "M")
-    assert_equal("medium", dog.size)
+    dog.age = ""
+    refute(dog.valid?, "Should be invalid with an empty age")
+
+    dog.age = nil
+    refute(dog.valid?, "Should be invalid without a age")
   end
+ 
+  # 
+  #---------------------------------------------------------  
+  def test_validator_should_fail_with_no_sex
+    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "10", sex: "M", good_with_kids: true)
+    assert(dog.valid?, "Should be valid at creation")
 
-  def test_age_getter
-    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "10", sex: "M")
-    assert_equal("10", dog.age)
+    dog.sex = ""
+    refute(dog.valid?, "Should be invalid with an empty sex")
+
+    dog.sex = nil
+    refute(dog.valid?, "Should be invalid without a sex")
   end
+ 
+  # 
+  #---------------------------------------------------------  
+  def test_validator_should_fail_with_no_good_with_kids
+    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "10", sex: "M", good_with_kids: true)
+    assert(dog.valid?, "Should be valid at creation")
 
-  def test_sex_written_getter
-    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "10", sex: "M")
-    assert_equal("M", dog.sex)
-  end
+    dog.good_with_kids = ""
+    refute(dog.valid?, "Should be invalid with an empty good_with_kids")
 
-  #SETTER TESTS
-  #---------------------------
-
-  def test_name_setter_with_valid_name
-    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "10", sex: "M")
-    dog.name = "Clementine"
-    assert_equal("Clementine", dog.name)
-  end
-
-  def test_breed_setter_with_valid_breed
-    dog = Dog.new(name: "Henry", breed: "UNKNOWN", size: "medium", age: "10", sex: "M")
-    dog.breed = "basset hound"
-    assert_equal("basset hound", dog.breed)
-  end
-
-  def test_size_setter_with_valid_size
-    dog = Dog.new(name: "Henry", breed: "basset hound", size: "small", age: "10", sex: "M")
-    dog.size = "medium"
-    assert_equal("medium", dog.size)
-  end
-
-  def test_age_setter_with_valid_age
-    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "1", sex: "M")
-    dog.age = "10"
-    assert_equal("10", dog.age)
-  end
-
-  def test_sex_setter_with_valid_sex
-    dog = Dog.new(name: "Henry", breed: "basset hound", size: "medium", age: "10", sex: "M")
-    dog.sex = "F"
-    assert_equal("F", dog.sex)
+    dog.good_with_kids = nil
+    refute(dog.valid?, "Should be invalid without a good_with_kids")
   end
 end
